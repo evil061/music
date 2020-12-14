9aa685294eb134f87359c96799170118f0ed26df
tushar061758 <73417554+tushar061758@users.noreply.github.com>
2020-12-14 10:40:49 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,14 +13,14 @@ module.exports = {
 
         client.commands.forEach(cmd => {
             let cmdinfo = cmd.info
-            allcmds+="``"+client.config.prefix+cmdinfo.name+" "+cmdinfo.usage+"`` ~ "+cmdinfo.description+"\n"
+            allcmds+="**"+client.config.prefix+cmdinfo.name+" "+cmdinfo.usage+"** ~ `"+cmdinfo.description +"`" +"\n"
         })
 
         let embed = new MessageEmbed()
         .setAuthor("Commands of "+client.user.username, "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
         .setColor("BLUE")
         .setDescription(allcmds)
-        .setFooter(`To get info of each command you can do ${client.config.prefix}help [command] | Hander by ItzCutePikachu#2006`)
+        .setFooter(`To get info of each command you can do ${client.config.prefix}help [command] | Hander by evil#8479`)
 
         if(!args[0])return message.channel.send(embed)
         else {
